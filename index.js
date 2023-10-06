eb966308057fe28fb49cfed8f540db2a3fb3ac56
rasmus <4j534j@gmail.com>
2023-10-06 09:35:19 +0300

    Add error management to prject

--- index.js
+++ index.js
@@ -8,15 +8,34 @@ app.set('views', path.join(__dirname, 'views'))
 const parseUrl = require("body-parser");
 let encodeUrl = parseUrl.urlencoded({ extended: true});
 
+let error = null
 app.get('/', (req, res) => {
-	res.render("page", {data: null})
+	res.render("page", {data: null, error: error})
 })
 
 
 const validId = require("./validate")
 
 app.post("/", encodeUrl, (req, res) => {
-	res.render("page", {data: validId.isikukood(req.body.id_code)})	
+	let error = null
+	if(req.body.id_code === ""){
+	error = "Palun sisesta vormis andmed"
+
+	} else if(req.body.id_code.length < 11){
+		error = "palun siste korrektne isikukood"
+
+	}
+	if(error === null){
+		res.render("page", {
+			data: validId.isikukood(req.body.id_code),
+			error: null
+		})
+
+	}else {
+		res.render("page", {data: null, error: error})
+	}
+
+	//res.render("page", {data: validId.isikukood(req.body.id_code)})	
 })
 
 app.listen(3000, () => {
